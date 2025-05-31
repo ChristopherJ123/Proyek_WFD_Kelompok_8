9dab0a11d5afc2f66c1298f23a812416069ad740
Ryzlock <ryan2tjiumena@gmail.com>
2025-05-31 09:52:37 +0700

    added migrations

---
+++ database/migrations/2025_05_30_164246_post.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('post', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('author_id')->constrained('user')->onDelete('cascade');
+            $table->foreignId('topic_id')->constrained('topic')->onDelete('cascade');
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->integer('share_count')->default(0);
+            $table->boolean('is_deleted')->default(false);
+            $table->string('moderator_notice')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('post');
+    }
+};
---
+++ database/migrations/2025_05_31_022640_user_post_report.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_post_report', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('post_id')->constrained()->onDelete('cascade');
+            $table->foreignId('post_comment_id')->nullable()->constrained('post_comment')->onDelete('cascade');
+            $table->string('report_reason');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_post_report');
+    }
+};
