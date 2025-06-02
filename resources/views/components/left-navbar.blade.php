e5882c8061e52def40aced1165bb20fbcd9bc442
ChristopherJ123 <christopherz2002@hotmail.com>
2025-06-02 12:35:12 +0700

    homepage mockup

--- resources/views/components/left-navbar.blade.php
+++ resources/views/components/left-navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="flex flex-col text-2xl font-bold tracking-wider bg-brand-900 text-brand-500 p-2 gap-4 max-w-64 w-full">
+<nav class="flex flex-col text-2xl font-bold tracking-wider bg-brand-900 text-brand-500 p-2 gap-4 max-w-64 w-full overflow-auto">
     <div class="flex items-end gap-2">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-8">
             <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
