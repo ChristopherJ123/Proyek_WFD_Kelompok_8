7f828772dd2fa759414cf1a5cb2220560215b684
ChristopherJ123 <christopherz2002@hotmail.com>
2025-06-21 14:44:42 +0700

    feat: tweak

--- resources/views/components/left-navbar.blade.php
+++ resources/views/components/left-navbar.blade.php
@@ -14,7 +14,7 @@
             -translate-x-full
             md:static md:translate-x-0">
     <button id="mobile‑nav‑toggle"
-            class="absolute top-32 -right-12 z‑50 flex items-center gap‑1 rounded-full bg-brand-900 text-brand-500 p-2
+            class="absolute top-32 -right-12 z‑50 flex md:hidden items-center gap‑1 rounded-full bg-brand-900 text-brand-500 p-2
                shadow-lg">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
